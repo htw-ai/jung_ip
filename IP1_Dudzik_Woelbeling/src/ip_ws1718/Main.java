0e8d021d9dc1605a59dc09beb37f5898b63fec86
Laura Woelbeling <s0548106@htw-berlin.de>
2017-10-12 21:19:07 +0200

    changed application title bar

--- IP1_Dudzik_Woelbeling/src/ip_ws1718/Main.java
+++ IP1_Dudzik_Woelbeling/src/ip_ws1718/Main.java
@@ -1,8 +1,6 @@
-// IP Ue1 WS2017/18 Vorgabe
+// IP Ue1 WS2017/18 
 //
-// Copyright (C) 2017 by Klaus Jung
-// All rights reserved.
-// Date: 2017-08-18
+// Date: 2017-10-12
 
 package ip_ws1718;
 	
@@ -19,7 +17,7 @@ public class Main extends Application {
 		BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("BinarizeView.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Binarize - <Your name>"); // TODO: add your name(s)
+		primaryStage.setTitle("Binarize - Dudzik/Woelbeling");
 		primaryStage.show();
 	}
 	
