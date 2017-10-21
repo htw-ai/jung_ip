9b3bdf289a6df7e9429bf7c0bbccab6a2120b9e8
Laura Woelbeling <s0548106@htw-berlin.de>
2017-10-21 16:55:04 +0200

    implemented erosion instead of dilation (less inverting in outline algorithm)

--- IP1_Dudzik_Woelbeling/src/ip_ws1718/Filter.java
+++ IP1_Dudzik_Woelbeling/src/ip_ws1718/Filter.java
@@ -13,8 +13,8 @@ public class Filter {
 	public static void outline(RasterImage src, RasterImage dst) {
 		// TODO: implement outline filter
 		//invert(src, dst);
-		dilate(src, dst);
-		//erode(src, dst);
+		//dilate(src, dst);
+		erode(src, dst);
 	}
 	
 	/**
@@ -43,7 +43,7 @@ public class Filter {
 	
 	// fest verdrahtetes Strukturelement
 	// erode und dilate sind vertauscht, weil Vorder- und Hintergrund vertauscht sind!
-	public static void dilate(RasterImage src, RasterImage dst) {
+	public static void erode(RasterImage src, RasterImage dst) {
 		int index, ind, valr, valg, valb;
 		checkDimen(dst, dst);
 		
@@ -53,7 +53,7 @@ public class Filter {
 				
 				ind = index = h * src.width + w;
 				// TODO: Rand!
-				valr = valg = valb = 255;	// set to maximum value i.o. to later find minimum!
+				valr = valg = valb = 0;	// set to minimum value i.o. to later find maximum!
 
 				// structured element with 4-neighbourhood: find maximum at 5 different positions
 				for (int i = 0; i < 5; i++) {
@@ -74,9 +74,9 @@ public class Filter {
 					default: 
 							index = h * src.width + w;
 					}
-					valr = min(((src.argb[index]>> 16) & 0xff) - 1, valr);
-					valg = min(((src.argb[index]>> 8) & 0xff) - 1, valg);
-					valb = min((src.argb[index] & 0xff) - 1, valb);
+					valr = max(((src.argb[index]>> 16) & 0xff) + 1, valr);
+					valg = max(((src.argb[index]>> 8) & 0xff) + 1, valg);
+					valb = max((src.argb[index] & 0xff) + 1, valb);
 				}
 				
 				// clamp values and write
@@ -85,38 +85,21 @@ public class Filter {
 		}
 	}
 	
-	// Erosion = Inversion der Dilatation des invertierten Bildes
-	public static void erode(RasterImage src, RasterImage dst) {
-		RasterImage inv = new RasterImage(src.width, src.height);
-		invert(src, inv);
-		
-		RasterImage dil = new RasterImage(src.width, src.height);
-		dilate(inv, dil);
-		
-		invert(dil, dst);
-	}
+	// Dilatation = Inversion der Erosion des invertierten Bildes
+		public static void dilate(RasterImage src, RasterImage dst) {
+			RasterImage inv = new RasterImage(src.width, src.height);
+			invert(src, inv);
+			
+			RasterImage erd = new RasterImage(src.width, src.height);
+			erode(inv, erd);
+			
+			invert(erd, dst);
+		}
 	
 	private static void checkDimen (RasterImage src, RasterImage dst) {
 		if ((src.height != dst.height) || (src.width != dst.width) ) 
 			throw new ArrayIndexOutOfBoundsException("Source and destination image do not match!");
 
-	}
-	
-	/**
-	 * helper: do all the shifting and masking to get red, green and blue and compute luminosity by adding these and dividing by 3
-	 * @param color	pixel value as int
-	 * @return gray level 0 - 255
-	 */
-	
-	private static int getGrayValue(int color) {
-		int mask = 0xff;
-		// return luminosity (gray value)
-		int red = (color >> 16) & mask;
-		int green = (color >> 8) & mask;
-		int blue = color & mask;
-		int lum = (red + green + blue) / 3;
-		
-		return lum;
 	}
 	
 	private static int max(int a, int b) {
